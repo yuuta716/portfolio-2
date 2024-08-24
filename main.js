91f0b0e2e953ffa27a23b550049278a7eb8f8c62
yuuta716 <139669904+yuuta716@users.noreply.github.com>
2024-08-24 21:52:56 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const menu = document.getElementById('menu');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  menu.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+}
